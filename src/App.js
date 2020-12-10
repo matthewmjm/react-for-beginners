2e777e8c814cf35a6b2ba67148c947e4798018d0
matthewmjm <matthewmjm@yahoo.com>
2020-12-09 21:34:01 -0700

    display menu and add to order functionality

--- src/App.js
+++ src/App.js
@@ -2,17 +2,51 @@ import React from 'react';
 import Header from './components/Header';
 import Inventory from './components/Inventory';
 import Order from './components/Order';
+import sampleFishes from './sample-fishes'
+import Fish from './components/Fish'
+import fishes from './sample-fishes';
 
 class App extends React.Component {
+
+  state = {
+    fishes: {},
+    order: {}
+  };
+
+  addFish = fish => {
+    const fishes = { ...this.state.fishes }
+    fishes[`fish${Date.now()}`] = fish
+    this.setState({fishes})
+  }
+
+  loadSampleFishes = () => {
+    this.setState({ fishes: sampleFishes })
+  }
+
+  addToOrder = (key) => {
+    const order = {...this.state.order}
+    order[key] = order[key] + 1 || 1
+    this.setState({ order })
+  }
 
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market" />
+          <ul className="fishes">
+            {Object.keys(this.state.fishes).map(key => (
+              <Fish 
+                key={key} 
+                index={key} 
+                details={this.state.fishes[key]} 
+                addToOrder={this.addToOrder}
+              />
+            ))}
+          </ul>
         </div>
         <Order />
-        <Inventory />
+        <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes}/>
       </div>
     );
   }
